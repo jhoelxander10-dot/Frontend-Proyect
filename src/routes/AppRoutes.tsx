376a4d65533286380942ff81689d8809f5ed1599
Jhoel Alexander <jhoelxander10@gmail.com>
2026-09-03 22:58:53 +0200

    Agregar rutas funcionales para alumnos cursos notas y horarios

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import AlumnosPage from "../pages/AlumnosPage";
+import CursosPage from "../pages/CursosPage";
+import NotasPage from "../pages/NotasPage";
+import HorariosPage from "../pages/HorariosPage";
 
 function AppRoutes() {
   return (
@@ -8,6 +12,10 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/alumnos" element={<AlumnosPage />} />
+        <Route path="/cursos" element={<CursosPage />} />
+        <Route path="/notas" element={<NotasPage />} />
+        <Route path="/horarios" element={<HorariosPage />} />
       </Routes>
     </BrowserRouter>
   );
