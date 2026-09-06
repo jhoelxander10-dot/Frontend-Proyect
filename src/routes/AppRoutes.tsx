23be3ea3509ad480be5a2b70d10ef36fa90492ae
Jhoel Alexander <jhoelxander10@gmail.com>
2026-09-06 21:37:27 +0200

    Limpieza: eliminar ruta innecesaria de alumnos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
-import AlumnosPage from "../pages/AlumnosPage";
 import CursosPage from "../pages/CursosPage";
 import NotasPage from "../pages/NotasPage";
 import HorariosPage from "../pages/HorariosPage";
@@ -11,9 +10,8 @@ function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/alumnos" element={<AlumnosPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/cursos" element={<CursosPage />} />
         <Route path="/notas" element={<NotasPage />} />
         <Route path="/horarios" element={<HorariosPage />} />
